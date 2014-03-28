b6e555239692a8994cadbd0ad90a00ccf1d54e63
Kenzie Togami <ket1999@gmail.com>
2014-03-28 07:15:26 -0700

    Update World.java
    Add space

--- src/main/java/net/minecraft/server/World.java
+++ src/main/java/net/minecraft/server/World.java
@@ -902,7 +902,7 @@ public abstract class World implements IBlockAccess {
         } else if (entity.getBukkitEntity() instanceof org.bukkit.entity.Projectile) {
             // Not all projectiles extend EntityProjectile, so check for Bukkit interface instead
             event = CraftEventFactory.callProjectileLaunchEvent(entity);
-        } else if(!(entity instanceof EntityPlayer)){
+        } else if(!(entity instanceof EntityPlayer)) {
             event = CraftEventFactory.callEntitySpawnEvent(entity, spawnReason);
         }
 
